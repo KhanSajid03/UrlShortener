7e8fcadeaf33ecff9f289fc9f874662dfd3f4f68
Sajid <sajid.khan.mn@gmail.com>
2025-04-21 23:29:50 -0500

    first

---
+++ url-shortener-sb/src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.url.shortener.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private  JwtUtils jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            // Get JWT from Header
+            String jwt = jwtTokenProvider.getJwtFromHeader(request);
+
+            if (jwt != null && jwtTokenProvider.validateToken(jwt)) {
+                String username = jwtTokenProvider.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            }
+            // Validate Token
+            // If Valid get user details
+                // -- get username --> load User -> Set the auth context
+        } catch (Exception e) {
+
+        }
+    }
+}
