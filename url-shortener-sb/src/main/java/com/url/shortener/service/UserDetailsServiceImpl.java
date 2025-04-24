b8bb4ecae25accd8587e29ba1f4092b2f8cd8103
Sajid <sajid.khan.mn@gmail.com>
2025-04-23 22:52:46 -0500

    Security Config

--- url-shortener-sb/src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
+++ url-shortener-sb/src/main/java/com/url/shortener/service/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.url.shortener.service;
 
 import com.url.shortener.models.User;
 import com.url.shortener.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
